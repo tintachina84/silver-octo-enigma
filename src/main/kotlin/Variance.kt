194ce343a501511e6891e1de88bdac5a6c9142ab
seonghyeon.seo <tintachina84@gmail.com>
2022-03-17 00:04:50 +0900

    Variant 1

--- src/main/kotlin/Variance.kt
+++ src/main/kotlin/Variance.kt
@@ -1,3 +1,17 @@
-fun main(args: Array<String>) {
+interface Box<T>
 
+open class Language
+open class JVM : Language()
+open class Kotlin2 : JVM()
+
+fun main(args: Array<String>) {
+    val languageBox = object : Box<Language> {}
+    val jvmBox = object : Box<JVM> {}
+    val kotlinBox = object : Box<Kotlin2> {}
+
+//    invariant(languageBox) // 컴파일 오류
+    invariant(jvmBox)
+//    invariant(kotlinBox) // 컴파일 오류
 }
+
+fun invariant(value: Box<JVM>) {}
